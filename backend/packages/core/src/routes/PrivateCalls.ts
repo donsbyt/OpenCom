29ed10f3570a79b55e2e3bf6714f975148f4e108
don <heisgenburgthecook@gmail.com>
2026-03-03 09:42:00 +0000

    Finish Call status

--- backend/packages/core/src/routes/PrivateCalls.ts
+++ backend/packages/core/src/routes/PrivateCalls.ts
@@ -2,6 +2,7 @@ import { q } from "../db.js";
 import type { FastifyInstance } from "fastify";
 
 // Error Code Syntax for future ref:
+// 420: Database Error
 // 421: Missing caller paramter
 // 422: Missing callid Paramater
 
@@ -16,6 +17,8 @@ export async function CallRoutes(app: FastifyInstance) {
 
         const call_id = req.body.callid;
 
+        const status = false
+
         if (!caller) {
             return rep.send({'error': true, 'code': 421} )
         }
@@ -23,5 +26,17 @@ export async function CallRoutes(app: FastifyInstance) {
             return rep.send({'error': true, 'code': 422} )
         }
 
+        // Preety simple db handalling should probably improve later
+        const existing = await q<{ status: boolean }>(
+            `SELECT status FROM private_calls WHERE caller=$1 AND call_id=$2`,
+            [caller, call_id]
+        );
+        // this might make up for it, is good enough for right now
+        if (existing.length) {
+            const status = existing[0].status;
+        } else {
+            return rep.send({'success': false, 'error': true, 'code': 420})
+        }
+        return rep.send({'success': true, 'status': status})
     });
 }
